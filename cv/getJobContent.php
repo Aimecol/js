6fc789522b0c6dfdfbd0c5e2e5f0092d41abeb6b
Aime Claudien Mazimpaka <152740405+Aimecol@users.noreply.github.com>
2024-11-29 04:05:44 +0200

    Delete cv directory

--- cv/getJobContent.php
+++
@@ -1,8 +0,0 @@
-<?php
-$job = $_GET['job'];
-$content = [
-    "marketingManager" => "Develop, implement, and oversee comprehensive marketing strategies to drive brand awareness, customer acquisition, and revenue growth. Conduct thorough market research to identify customer needs, preferences, and trends.",
-    "salesExecutive" => "Identify and prospect potential clients through research, cold calling, networking, and referrals. Build and nurture strong client relationships to understand their needs and deliver tailored solutions."
-];
-
-echo isset($content[$job]) ? $content[$job] : "Job content not found.";
